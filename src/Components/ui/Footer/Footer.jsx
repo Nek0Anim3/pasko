f18621ec832513fac3c100aee3d795ec3128a7ef
Cenaure <127963429+Cenaure@users.noreply.github.com>
2024-09-23 23:16:47 +0300

    Update Footer.jsx

--- src/Components/ui/Footer/Footer.jsx
+++ src/Components/ui/Footer/Footer.jsx
@@ -1,9 +1,5 @@
 import Image from 'next/image';
 import styles from './footer.css';
-import leader from '../assets/leaderboard.png';
-import mine from '../assets/mine.png';
-import invite from '../assets/invite.png';
-import task from '../assets/tasks.png';
 import Link from 'next/link';
 
 export default function FooterBar() {
@@ -18,25 +14,25 @@ export default function FooterBar() {
 
         <Link href="/leader" passHref>
           <button className={style.footerButton}>
-            <Image src={leader} width={40} height={40} alt="Leader Icon" />
+            <Image src={'/leaderboard.png'} width={40} height={40} alt="Leader Icon" />
           </button>
         </Link>
 
         <Link href="/" passHref>
           <button className={style.footerButton}>
-            <Image src={mine} width={40} height={40} alt="Mine Icon" />
+            <Image src={'/mine.png'} width={40} height={40} alt="Mine Icon" />
           </button>
         </Link>
 
         <Link href="/invite" passHref>
           <button className={style.footerButton}>
-            <Image src={invite} width={40} height={40} alt="Invite Icon" />
+            <Image src={'/invite.png'} width={40} height={40} alt="Invite Icon" />
           </button>
         </Link>
 
         <Link href="/task" passHref>
           <button className={style.footerButton}>
-            <Image src={task} width={40} height={40} alt="Task Icon" />
+            <Image src={'/tasks.png'} width={40} height={40} alt="Task Icon" />
           </button>
         </Link>
       </div>
