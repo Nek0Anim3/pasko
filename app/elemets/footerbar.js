d0cb4be6a6f67253040458998e842e26b05800ad
Nek0Anim3 <pogoderopa@gmail.com>
2024-09-23 19:06:41 +0300

    Update footerbar.js

--- app/elemets/footerbar.js
+++ app/elemets/footerbar.js
@@ -1,18 +1,32 @@
 
 import Image from 'next/image';
 import './footer.css';
-import upgrade from '../assets/upgrade.png'
+import upgrade from '../assets/upgrade.png';
+import leader from '../assets/leaderboard.png';
+import mine from '../assets/mine.png';
+import invite from '../assets/invite.png';
+import task from '../assets/tasks.png';
 
 
 export default function FooterBar() {
   return (
     <main>
         <div className="foot-cont">
-            <button className='upgrade-but'><Image src={upgrade} width={40} height={40}></Image></button>
-            <button className='leader-but'>LE</button>
-            <button className='mine-but'>MINE</button>
-            <button className='invite-but'>IN</button>
-            <button className='task-but'>TA</button>
+        <button className="upgrade-but">
+          <Image src={upgrade} width={40} height={40} alt="Upgrade Icon" />
+        </button>
+        <button className="leader-but">
+          <Image src={leader} width={40} height={40} alt="Leader Icon" />
+        </button>
+        <button className="mine-but">
+          <Image src={mine} width={40} height={40} alt="Mine Icon" />
+        </button>
+        <button className="invite-but">
+          <Image src={invite} width={40} height={40} alt="Invite Icon" />
+        </button>
+        <button className="task-but">
+          <Image src={task} width={40} height={40} alt="Task Icon" />
+        </button>
         </div>
     </main> 
 
