89377c54632ef24bd42baa2f42ba01c7c0d274cf
Cenaure <127963429+Cenaure@users.noreply.github.com>
2024-09-24 00:50:16 +0300

    Update layout.jsx

--- src/app/(Routes)/layout.jsx
+++ src/app/(Routes)/layout.jsx
@@ -1,19 +1,13 @@
 import Footer from "@/src/Components/ui/Footer/Footer";
 import Header from "@/src/Components/ui/Header/Header";
-import { getUserData } from "@/src/utils/getUserData";
 
 
 export default function Layout({ children }) {
-
-  let username = getUserData().username
-
   return (
     <>
       <Header />
       <div className="content">
         {children}
-
-        {username}
       </div>
       <Footer />
     </>
