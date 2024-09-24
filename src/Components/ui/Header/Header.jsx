e19dceb9e52765bc74b162fd713dca431b3cfdb3
Cenaure <127963429+Cenaure@users.noreply.github.com>
2024-09-24 23:23:47 +0300

    Update Header.jsx

--- src/Components/ui/Header/Header.jsx
+++ src/Components/ui/Header/Header.jsx
@@ -57,11 +57,6 @@ const Header = () => {
 
   }, []); // Запускаем только один раз при монтировании
 
-  useEffect(() => {
-    
-  }, []);
-  
-
   return (
     <header className={styles.header}>
       <div className={styles.statsContainer}>
