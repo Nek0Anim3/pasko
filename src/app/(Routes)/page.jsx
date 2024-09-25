2837251058028c64f92bb01f49c5511c13b37dcf
Cenaure <127963429+Cenaure@users.noreply.github.com>
2024-09-25 23:07:43 +0300

    1

--- src/app/(Routes)/page.jsx
+++ src/app/(Routes)/page.jsx
@@ -4,10 +4,7 @@ export default function Farm() {
   return  (
     <main>
       <div className={styles.clickerContent}>
-        <div className={styles.clickerContainer}>
-          <button className={styles.paskoEblet}>123</button>
-          <p className={styles.pointsText}>2817329</p>
-        </div>
+        <div className="clicker-container"></div>
       </div>
     </main>
   )
