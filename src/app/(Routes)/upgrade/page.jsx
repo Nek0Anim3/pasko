e76fc60c0f836eb16809f864a11174e14090ea6f
Cenaure <127963429+Cenaure@users.noreply.github.com>
2024-09-27 18:53:07 +0300

    Update page.jsx

--- src/app/(Routes)/upgrade/page.jsx
+++ src/app/(Routes)/upgrade/page.jsx
@@ -1,25 +1,16 @@
 "use client";
 
-import BackButtonComponent from "@/src/Components/ui/BackButtonComponent/BackButtonComponent";
 import { Typography } from "@mui/material";
-import { SDKProvider } from "@telegram-apps/sdk-react";
-import { useEffect, useState } from "react";
+
 
 const Upgrades = () => {
-  const [isTelegramApp, setIsTelegramApp] = useState(true);
+  //const [isTelegramApp, setIsTelegramApp] = useState(true);
 
   return (
     <>
-      {isTelegramApp ? (
-        <SDKProvider acceptCustomStyles debug>
-          <Typography variant="h3" component="p">Coming soon!</Typography>
-          <BackButtonComponent />
-        </SDKProvider>
-      ) : (
-        <Typography variant="h5" component="p">
-          Coming soon!
-        </Typography>
-      )}
+
+      <Typography variant="h3" component="p">Coming soon!</Typography>
+
     </>
   );
 };
