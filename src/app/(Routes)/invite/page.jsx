0ed12084015edac4865be2beabccd4f93cd1ea82
Cenaure <127963429+Cenaure@users.noreply.github.com>
2024-09-27 18:29:55 +0300

    pages

---
+++ src/app/(Routes)/invite/page.jsx
@@ -0,0 +1,7 @@
+import { Typography } from "@mui/material"
+
+const Invitation = () => {
+  return <Typography variant="h3" component="p">Coming soon!</Typography>
+}
+
+export default Invitation
---
+++ src/app/(Routes)/leader/page.jsx
@@ -0,0 +1,7 @@
+import { Typography } from "@mui/material"
+
+const Leaderboard = () => {
+  return <Typography variant="h3" component="p">Coming soon!</Typography>
+}
+
+export default Leaderboard
