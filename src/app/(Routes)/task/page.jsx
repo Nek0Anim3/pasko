003598d6df306d23850aa362d2be53b5a70ab12c
Cenaure <127963429+Cenaure@users.noreply.github.com>
2024-09-27 18:14:22 +0300

    Update page.jsx

--- src/app/(Routes)/task/page.jsx
+++ src/app/(Routes)/task/page.jsx
@@ -2,8 +2,6 @@ import { Typography } from "@mui/material"
 import { initBackButton } from "@telegram-apps/sdk";
 
 const Task = () => {
-  const [backButton] = initBackButton();
-  backButton.show();
   return <Typography variant="h3" component="p">Coming soon!</Typography>
 }
 
