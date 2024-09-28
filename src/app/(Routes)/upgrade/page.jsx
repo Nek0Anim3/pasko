5980065c74dd4df5b13029166d55c6ac9f73cdd3
Cenaure <127963429+Cenaure@users.noreply.github.com>
2024-09-28 21:34:15 +0300

    Transitions

--- src/app/(Routes)/upgrade/page.jsx
+++ src/app/(Routes)/upgrade/page.jsx
@@ -5,11 +5,16 @@ const Upgrades = () => {
     
 
 
-    return (
-      <div className={`upgradeContent ${styles.upgradesPageContainer}`}>
-          <Typography variant="h3" component="h1" className={styles.title}>Upgrades</Typography>
+  return (
+    <div className={`upgradeContent ${styles.upgradesPageContainer}`}>
+
+      <div className="upgradePageHeader">
+        Pasko Anatoliy
       </div>
-    );
+
+      <Typography variant="h3" component="h1" className={styles.title}>Upgrades</Typography>
+    </div>
+  );
 };
 
 export default Upgrades;
