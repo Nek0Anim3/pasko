597c6942a1b6e6a067e62a8f4edec5f331f1860a
Cenaure <127963429+Cenaure@users.noreply.github.com>
2024-10-02 22:28:50 +0300

    Update Header.jsx

--- src/Components/ui/Header/Header.jsx
+++ src/Components/ui/Header/Header.jsx
@@ -91,7 +91,7 @@ const Header = () => {
                 }}
               /> // Заглушка для аватара
             )}
-            <ScrollNickname nick={userData?.tgUser?.nickname || 'Anonymous'}></ScrollNickname>                         {/*<h1 style={{fontWeight: 100}}></h1>   {userData?.tgUser?.firstName || 'Guest'}*/}
+            <ScrollNickname nick={userData?.tgUser?.username || 'Anonymous'}></ScrollNickname>                         {/*<h1 style={{fontWeight: 100}}></h1>   {userData?.tgUser?.firstName || 'Guest'}*/}
           </div>
           <div className={styles.place}>
             <h1>#1</h1>
