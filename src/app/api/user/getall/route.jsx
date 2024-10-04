798a39cb30a28c3a08da69c67ff52335fda770fd
Cenaure <127963429+Cenaure@users.noreply.github.com>
2024-10-04 17:28:18 +0300

    Update route.jsx

--- src/app/api/user/getall/route.jsx
+++ src/app/api/user/getall/route.jsx
@@ -1,22 +1,36 @@
 import { NextResponse } from 'next/server'; // Используем NextResponse
 import { connectToDatabase } from '@/src/lib/db'; // Подключение к MongoDB клиенту
+import NodeCache from 'node-cache'; // Импортируем node-cache
+
+// Создаем экземпляр кэша с таймаутом жизни кэша (TTL) 60 секунд
+const cache = new NodeCache({ stdTTL: 60, checkperiod: 120 });
 
 export async function POST() {
   try {
-    // Подключение к базе данных
+    // Попробуем получить данные из кэша
+    const cachedUsers = cache.get('leaderboard');
+
+    if (cachedUsers) {
+      console.log(1)
+      return NextResponse.json({ users: cachedUsers });
+    }
+
+    // Если данных нет в кэше, подключаемся к базе данных
     const { database } = await connectToDatabase();
 
     // Получаем данные из базы данных
     const users = await database
-      .collection("users")
+      .collection('users')
       .find({}, { projection: { uid: 1, firstName: 1, maxPoints: 1, avatarUrl: 1 } })
       .sort({ maxPoints: -1 }) // Сортируем по количеству очков
       .limit(20) // Ограничиваем список до топ-20 пользователей
       .toArray();
 
+    // Сохраняем данные в кэш на 60 секунд
+    cache.set('leaderboard', users);
+
     // Возвращаем данные пользователям
     return NextResponse.json({ users });
-
   } catch (error) {
     console.error('Error fetching leaderboard data:', error);
     return NextResponse.json({ error: 'Failed to fetch leaderboard data' }, { status: 500 });
